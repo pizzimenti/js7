4d28a4b7d54567bdfc3727317e86dff29a6547f4
Brad Pizzimenti and Ramon de la Cruz <virgilioramon.delacruz@gmail.com>
2016-03-22 09:27:27 -0700

    add display of tasks

---
+++ agile.md
@@ -0,0 +1,12 @@
+## Doing
+
+As a user, I want a task to have a description, and I need to know if it's done or not.
+As a user, I want to be able to edit existing task descriptions and create new tasks
+As a user, I want to be able to view which tasks are done, and which tasks are not done.
+As a user, I want tasks to start out set to "not done". But I want to be able to click a checkbox to mark them as "done". I would also like the ability to uncheck a box to mark the tasks as "not done", in case I realize I messed one of them up and I have to do it over.
+
+## Done
+
+As a user, I want to be able to see all my tasks
+
+## Backlog
--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,33 @@
 import { Component } from 'angular2/core';
 
+
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+  <div class="container">
+  <h1>To-Do List</h1>
+  <h3 *ngFor="#task of tasks">{{ task.description }}</h3>
+  </div>
   `
 })
 
-export class AppComponent {
 
+export class AppComponent {
+  public tasks: Task[];  //Task[] (or Array<Task>) identifies tasks as an array of objects
+  constructor(){
+    this.tasks = [
+      new Task("Create To-Do List app", 0),
+      new Task("Learn Kung Fu", 1),
+      new Task("Rewatch Lord of the Rings", 2),
+      new Task("Do the Laundry", 3)
+    ];
+  }
+}
+
+
+export class Task {
+  public done: boolean = false;
+  constructor(public description: string, public id: number){
+
+  }
 }
